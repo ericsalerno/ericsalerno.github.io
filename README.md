f8fcd591afee9146894e5c0a146a80860558f16c
Eric Salerno <salernolabs@gmail.com>
2023-06-27 12:14:35 -0400

    honor cookies in gtag

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ https://www.ericsalerno.com/
 
 Not much to read here, sorry. This is just a github page on my vanity domain.
 
-This website uses `ihavecookies.js` for GDPR cookies: https://github.com/ketanmistry/ihavecookies
+## This website uses 
+
+ * `jQuery` https://jquery.com/
+ * `js-cookie/js-cookie` https://github.com/js-cookie/js-cookie
+ * `ihavecookies.js` https://github.com/ketanmistry/ihavecookies
--- resources/main.js
+++ resources/main.js
@@ -1,10 +1,32 @@
 /**
  * Hello!
  */
-$(document).ready(function() {
+function debug(message) {
     if (typeof console != 'undefined') {
-        console.log('Hello friend!');
+        console.log(message);
     }
+}
+function initializeGa4(disableAnalyticsCookies = false) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    if (disableAnalyticsCookies) {
+        debug('gtag minus cookies');
+        gtag("consent", "default", {
+            ad_storage: "denied",
+            analytics_storage: "denied",
+            functionality_storage: "denied",
+            personalization_storage: "denied",
+            security_storage: "denied"
+        });
+    }
+
+    gtag('config', 'G-L4Q3RWN7DP');
+}
+
+$(document).ready(function() {
+    debug('Hello friend');
 
     var options = {
         title:"Cookies & Privacy",
@@ -16,4 +38,9 @@ $(document).ready(function() {
     $('#update-preferences').click(function(){
         $('body').ihavecookies(options, 'reinit');
     });
+
+    var cookieControl = JSON.parse(Cookies.get('cookieControlPrefs') || "['analytics']");
+    var disallowAnalyticsCookies = cookieControl.includes('analytics') === false;
+
+    initializeGa4(disallowAnalyticsCookies);
 });
