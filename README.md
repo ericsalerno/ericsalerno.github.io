2ff12eff31a6fb1db1d312ba3eccdd3a3ffc8b8a
Eric Salerno <salernolabs@gmail.com>
2023-06-27 11:49:57 -0400

    ihavecookies.js and privacy notice

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 https://www.ericsalerno.com/
 
 Not much to read here, sorry. This is just a github page on my vanity domain.
+
+This website uses `ihavecookies.js` for GDPR cookies: https://github.com/ketanmistry/ihavecookies
--- resources/main.js
+++ resources/main.js
@@ -1,4 +1,19 @@
-/* Nothing here, lol <3 */
-if (typeof console != 'undefined') {
-    console.log('Hello friend!');
-}
+/**
+ * Hello!
+ */
+$(document).ready(function() {
+    if (typeof console != 'undefined') {
+        console.log('Hello friend!');
+    }
+
+    var options = {
+        title:"Cookies & Privacy",
+        message:"This website uses cookies to ensure you get the best experience on our website.",
+        link:"https://www.salernolabs.com/privacy",
+        delay: 250,
+    };
+    $('body').ihavecookies(options);
+    $('#update-preferences').click(function(){
+        $('body').ihavecookies(options, 'reinit');
+    });
+});
